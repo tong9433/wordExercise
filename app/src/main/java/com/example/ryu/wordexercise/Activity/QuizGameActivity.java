aed2fd38eb5ea23e652eab80449a6cd0081266b4
Tongil Song <tong9433@naver.com>
2017-03-28 19:39:22 +0900

    음성합성 2차수정
    2017.3.28

--- app/src/main/java/com/example/ryu/wordexercise/Activity/QuizGameActivity.java
+++ app/src/main/java/com/example/ryu/wordexercise/Activity/QuizGameActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +20,9 @@ import com.example.ryu.wordexercise.TTS;
 public class QuizGameActivity extends Activity {
 
     private EditText editText;
+    private Button button;
+    private Button playButton;
+    private TTS tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +31,24 @@ public class QuizGameActivity extends Activity {
         setContentView(R.layout.activity_quizgame);
 
         editText =(EditText) findViewById(R.id.edit_query);
-        Button button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
+        playButton = (Button) findViewById(R.id.playbutton);
+
+        tts=new TTS(QuizGameActivity.this,playButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TTS tts=new TTS(QuizGameActivity.this,v,editText);
+
+                String message=editText.getText().toString();;
+                if (!TextUtils.isEmpty(message)) {
+
+                    tts.requestNaverApi(message);
+                    Log.e("sds","sdsd");
+                }
+
             }
         });
-
-
-
-
-
     }
 
 
