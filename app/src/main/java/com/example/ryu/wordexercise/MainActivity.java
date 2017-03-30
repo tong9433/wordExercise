ef237e5a0e6a3629f58bd7408994d3442a96852d
blizzard7891 <bllizard7891@gmail.com>
2017-03-30 20:30:59 +0900

    translate

--- app/src/main/java/com/example/ryu/wordexercise/MainActivity.java
+++ app/src/main/java/com/example/ryu/wordexercise/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.ryu.wordexercise;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
 import java.io.InputStream;
 
 import io.realm.Realm;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     Realm realm;
     Word word;
+    Context mContext = this;
 
     Button bt_recognize;
     Button bt_translate;
@@ -40,9 +43,17 @@ public class MainActivity extends AppCompatActivity {
         bt_translate = (Button) findViewById(R.id.bt_translate);
         bt_textToSpeech = (Button) findViewById(R.id.bt_textToSpeech);
 
-        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         listView.setAdapter(arrayAdapter);
         copyExcelToDatabase();
+
+        bt_translate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, TranslateActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -54,30 +65,28 @@ public class MainActivity extends AppCompatActivity {
             workbookSettings.setEncoding("euc-kr");
             workbook = Workbook.getWorkbook(inputStream, workbookSettings);
 
-            if( workbook != null) {
+            if (workbook != null) {
                 Log.d("check1", "workbook is not null");
                 sheet = workbook.getSheet(0);
-                if( sheet != null) {
+                if (sheet != null) {
                     Log.d("check1", "sheet is not null");
                     realm = Realm.getDefaultInstance();
                     realm.executeTransaction(new Realm.Transaction() {
                         @Override
                         public void execute(Realm realm) {
-                            for(int i=0; i<800; i++) {
+                            for (int i = 0; i < 800; i++) {
                                 word = realm.createObject(Word.class);
                                 word.setWord(sheet.getCell(0, i).getContents().toString());
-                                Log.d("Word UTF checking", ""+word.getWord());
-                                word.setMean(sheet.getCell(1,i).getContents().toString());
-                                Log.d("Mean UTF checking", ""+word.getMean());
+                                Log.d("Word UTF checking", "" + word.getWord());
+                                word.setMean(sheet.getCell(1, i).getContents().toString());
+                                Log.d("Mean UTF checking", "" + word.getMean());
                             }
                         }
                     });
-                }
-                else {
+                } else {
                     Log.d("check1", "sheet is null");
                 }
-            }
-            else {
+            } else {
                 Log.d("check", "workbook is null");
             }
         } catch (Exception e) {
@@ -85,11 +94,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         final RealmResults<Word> results = realm.where(Word.class).findAll();
-        Log.d("result size : ", ""+results.size());
+        Log.d("result size : ", "" + results.size());
         for (int i = 0; i < results.size(); i++) {
             String word = results.get(i).getWord().toString();
             String mean = results.get(i).getMean().toString();
-            String result = word +" : "+mean;
+            String result = word + " : " + mean;
             arrayAdapter.add(result);
         }
     }
